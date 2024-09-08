ca8ac5b2bf8db9b7a95d1ffa47e2eda82cc5ca21
IbragimCoder <ibragimabbasov05@gmail.com>
2024-09-08 11:47:31 +0400

    created the function "def my_matrix(n)" n is the size of the matrix (NxN), which finds the center of the square matrix and, moving clockwise from the center, increases the sequence until the matrix is completed

---
+++ EX2.py
@@ -0,0 +1,47 @@
+
+### PART 1
+
+print("PART 1\n")
+
+def my_matrix(n):
+    matrix = [[0] * n for _ in range(n)]
+
+    x = (n - 1) // 2
+    y = (n - 1) // 2
+    num = 1
+
+    direct = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    indexOfDirect = 0
+
+    step = 1
+    i = 0
+
+    while num <= n ** 2:
+        while 1:
+            for j in range(step):
+                if (0 <= x and x < n) and (0 <= y and y < n):
+                    matrix[x][y] = num
+                    num = num + 1
+
+                direction = direct[indexOfDirect]
+
+                x_direct = direction[0]
+                y_direct = direction[1]
+
+                x = x + x_direct
+                y = y + y_direct
+
+            if indexOfDirect == 3:
+                indexOfDirect = 0
+            else:
+                indexOfDirect += 1
+
+            i = i + 1
+
+            if i == 2:
+                i = 0
+                break
+
+        step += 1
+
+    return matrix
